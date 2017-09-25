07d1e27aa3a9ed4a546fa9a262b4bba63cfb2dd4
Metaphysiker <s.raess@me.com>
2017-09-25 23:57:05 +0200

    added 4 ex

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -16,5 +16,18 @@ class CoursesController < ApplicationController
         ["Lisa holt die Feuerwehr, obwohl gar kein Feuer brennt.", "bad", "Wahrscheinlich nicht. Wegen Lisa wurde unnötigerweise eine ganze Mannschaft in Stress versetzt und im schlimmsten Fall wird die Feuerwehr woanders benötigt."],
         ["Tamara erfährt, dass der Weg über die Brücke gesperrt ist. Auf dem Rückweg informiert sie andere Passanten, die auch über die Brücke wollten, dass der Weg über die Brücke gesperrt ist.", "good", "Wahrscheinlich nicht. Dank Tamara können sich die anderen Passanten die Mühe sparen, den Weg zur Brücke zu machen."]
     ]
+
+  @problems = [
+    "Der Konsequentialismus hat zu wenige Anhänger, um ernst genommen zu werden.",
+    "Der Konsequentialismus behauptet, dass wir immer lügen und stehlen sollen.",
+    "Wir können die Folgen unserer Handlungen nicht immer vorhersehen oder abschätzen.",
+    "Der Konsequentialismus zieht Absichten und Intentionen nicht in Erwägung.",
+    "Da die Willensfreiheit eine Illusion ist, kann man sich gar nicht für eine Handlung entscheiden.",
+    "Früher oder später hat jede Handlung gute Folgen, deshalb spielt es keine Rolle, wie man sich entscheidet.",
+    "In gewissen Situationen verlangt der Konsequentialismus, dass wir lügen, stehlen oder morden sollen.",
+    "Der Konsequentialismus verlangt, dass wir vor jeder Entscheidung zu einer Wahrsagerin gehen.",
+    "Der Konsequentialismus impliziert, dass jede Ursache eine Wirkung haben muss."
+  ]
+
   end
 end
